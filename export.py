d39c1c52bd0af074467641e5c29c843f1ccc6378
catonrug <aigars.kadikis@gmail.com>
2023-06-27 14:52:08 +0300

    host and all SNMP credentials

--- export.py
+++ export.py
@@ -36,7 +36,7 @@ import csv
 from pprint import pprint
 
 # prepare a file to write
-data_file = open('/tmp/jsonoutput.csv', 'w', newline='')
+data_file = open('/tmp/hosts.csv', 'w', newline='')
 csv_writer = csv.writer(data_file)
 
 
@@ -55,35 +55,112 @@ listOfHosts = parse('$.result').find(json.loads(requests.request("POST", url, he
     "jsonrpc": "2.0",
     "method": "host.get",
     "params": {
-        "output":["host","hostid","status"]
+        "output":["host","hostid","status","maintenance_status"],
+        "selectItems": "count",
+        "selectParentTemplates": ["name"],
+        "selectTriggers": "count"
         
     },
     "auth": token,
     "id": 1
 }), verify=False).text))[0].value
 
-# rename elements in JSON tree
+# rename elements in JSON tree to not conflict while mapping with other result
 for item in listOfHosts:
   item["hostName"] = item.pop("host")
   item["hostStatus"] = item.pop("status")
+  item["amountOfItems"] = item.pop("items")
+  item["amountOfTriggers"] = item.pop("triggers")
+  if len(item["parentTemplates"])>0:
+      print("there are",len(item["parentTemplates"]),"templates linked to ",item["hostName"])
+  # remove parentTemplates. This step does not make sence
+  item.pop("parentTemplates")
 
-# get list of interfaces
+# get list of interfaces. pick up only the "main" ones
 listOfInterfaces = parse('$.result').find(json.loads(requests.request("POST", url, headers=headers, data=json.dumps({
     "jsonrpc": "2.0",
     "method": "hostinterface.get",
     "params": {
-        "output": ["hostid","ip","dns","type"],
+        "output": ["hostid","ip","dns","type","details","port"],
         "filter": {"main":1}
     },
     "auth": token,
     "id": 1
 }), verify=False).text))[0].value
 
-# rename elements in JSON tree
+# rename elements in JSON tree to not conflict while mapping with other result
 for item in listOfInterfaces:
   item["interface_DNS"] = item.pop("dns")
   item["IP_address"] = item.pop("ip")
+  item["interfaceDetails"] = item.pop("details")
+  item["interfaceType"] = item.pop("type")
+  item["interfacePort"] = item.pop("port")
+  
+  # per SNMPv2/SNMPv3 the amount of columns differ, let's have them all in output
+  if len(item["interfaceDetails"])>0:
+      print("there are extra details per interface",item.get("hostid"),", that is:",item["interfaceDetails"])
+      try:
+          item["authpassphrase"] = item["interfaceDetails"]['interfaceDetails']
+      except:
+          item["authpassphrase"] = ""
+          
+      try:
+          item["authprotocol"] = item["interfaceDetails"]['authprotocol']
+      except:
+          item["authprotocol"] = ""
 
+      try:
+          item["bulk"] = item["interfaceDetails"]['bulk']
+      except:
+           item["bulk"] = ""
+
+      try:
+          item["contextname"] = item["interfaceDetails"]['contextname']
+      except:
+          item["contextname"] = ""
+
+      try:
+          item["privpassphrase"] = item["interfaceDetails"]['privpassphrase']
+      except:
+          item["privpassphrase"] = ""
+
+      try:
+          item["privprotocol"] = item["interfaceDetails"]['privprotocol']
+      except:
+          item["privprotocol"] = ""
+
+      try:
+          item["securitylevel"] = item["interfaceDetails"]['securitylevel']
+      except:
+          item["securitylevel"] = ""
+
+      try:
+          item["securityname"] = item["interfaceDetails"]['securityname']
+      except:
+          item["securityname"] = ""
+
+      try:
+          item["version"] = item["interfaceDetails"]['version']
+      except:
+          item["version"] = ""
+
+      #destroy original entity
+      item.pop("interfaceDetails")
+  else:
+      item["authpassphrase"] = ""
+      item["authprotocol"] = ""
+      item["bulk"] = ""
+      item["contextname"] = ""
+      item["privpassphrase"] = ""
+      item["privprotocol"] = ""
+      item["securitylevel"] = ""
+      item["securityname"] = ""
+      item["version"] = ""
+
+      # destroy original entity
+      item.pop("interfaceDetails")
+
+# merge 2 different lists together. It works like a magic and automatically locates the column name to do the mapping
 outcome = [json[0] | json[1] for json in zip(listOfHosts, listOfInterfaces)]
 pprint(outcome)
 
