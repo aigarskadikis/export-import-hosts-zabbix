3d83a50f68506bce68b9474c490ca28c851a38c0
catonrug <aigars.kadikis@gmail.com>
2023-06-27 09:13:58 +0300

    next

---
+++ export.py
@@ -0,0 +1,15 @@
+#!/bin/env python3.9
+
+# cat /var/lib/zabbix/config.py
+# url = "http://127.0.0.1/api_jsonrpc.php"
+# username = "Admin"
+# password = "zabbix"
+
+import sys
+sys.path.insert(0,'/var/lib/zabbix')
+import config
+
+print(config.url)
+print(config.username)
+print(config.password)
+
