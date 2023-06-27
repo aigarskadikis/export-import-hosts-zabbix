6e15b8cf9ef6f9b24bb7bca5f055a8813b2bf760
catonrug <aigars.kadikis@gmail.com>
2023-06-27 09:49:44 +0300

    next

--- export.py
+++ export.py
@@ -8,8 +8,39 @@
 import sys
 sys.path.insert(0,'/var/lib/zabbix')
 import config
+#print(config.url)
+#print(config.username)
+#print(config.password)
 
-print(config.url)
-print(config.username)
-print(config.password)
+# work with Zabbix API JSON RPC
+import requests
+import json
+
+# supress warnings in case of self signed certificate:
+# /usr/local/lib/python3.9/site-packages/urllib3/connectionpool.py:1095: InsecureRequestWarning: Unverified HTTPS request is being made to host 'zabbix.aigarskadikis.com'. Adding certificate verification is strongly advised. See: https://urllib3.readthedocs.io/en/latest/advanced-usage.html#tls-warnings
+import urllib3
+urllib3.disable_warnings()
+
+# shortify vairables
+url = config.url
+user = config.username
+password = config.password
+
+# have support for JSON path
+# pip3.9 install jsonpath-ng
+import json
+from jsonpath_ng import jsonpath, parse
+
+
+# pick up token which will be used latter in script
+payload = json.dumps({"jsonrpc":"2.0","method":"user.login","params":{"user":user,"password":password},"id":1})
+headers = {'Content-Type': 'application/json'}
+
+response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+
+#token = json.dumps(response.text).result
+
+#print(response.text)
+token = parse('$.result').find(json.loads(response.text))[0].value
+print(token)
 
