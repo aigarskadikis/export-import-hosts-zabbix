98b152fc6c02aeab41232bc6879176f59d410cd8
catonrug <aigars.kadikis@gmail.com>
2023-06-30 10:51:20 +0300

    optimize implemented

--- hosts-import.py
+++ hosts-import.py
@@ -72,167 +72,119 @@ listOfExistingHosts = parse('$.result').find(json.loads(requests.request("POST",
     "id": 1
 }), verify=False).text))[0].value
 
-# create a nice list which contains only hostnames. nothing else
-existingHostsList = []
-for existingHost in listOfExistingHosts:
-    existingHostsList.append(existingHost["host"])
 
-# go through all registred hosts in the destination instance
-for existingHost in listOfExistingHosts:
+# take new host
+for newHost in listOfHosts:
+    # by default lets assume host does not exist in destination
+    hostExists=0
+    # cehck if this host name already exists in destination. copmare 1 hostname with all hostnames in destination
+    for existingHost in listOfExistingHosts:
+        if existingHost["host"]==newHost["hostName"]:
+            hostExists=1
+            break
 
-    # take data from CSV
-    for newHost in listOfHosts:
+    if hostExists:
+        print(bcolors.OKGREEN + "'" + newHost["hostName"] + "' already exists in destination"+ bcolors.ENDC)
+    else:
+        # need to register new host
+        print(bcolors.FAIL + "'"+newHost["hostName"] + "' is not yet registred")
 
-        # check if object from CSV exists in the short host list
-        if newHost["hostName"] in existingHostsList:
-            print(bcolors.OKGREEN + "'" + newHost["hostName"] + "' already exists in destination"+ bcolors.ENDC)
-        else:
-            # need to register new host
-            print(bcolors.FAIL + "'"+newHost["hostName"] + "' is not yet registred")
+        # define new list of macros which is about to be installed on this host
+        newHostMacros = []
 
-            # define new list of macros which is about to be installed on this host
-            newHostMacros = []
+        # there can be mulitple lines in the list which match hostname
+        for macro in listOfHostMacros:
+            #print(macro["macro"])
+            if macro["hostName"] == newHost["hostName"]:
+                #print("found one matching line in macros.csv")
+                # a host can have multiple macros
+                row = {}
+                # all columns must exist in CSV
+                row["macro"] = macro["macro"]
+                row["description"] = macro["description"]
+                row["value"] = macro["value"]
+                row["type"] = macro["type"]
+                newHostMacros.append(row)
 
-            # there can be mulitple lines in the list which match hostname
-            for macro in listOfHostMacros:
-                #print(macro["macro"])
-                if macro["hostName"] == newHost["hostName"]:
-                    #print("found one matching line in macros.csv")
-                    # a host can have multiple macros
-                    row = {}
-                    # all columns must exist in CSV
-                    row["macro"] = macro["macro"]
-                    row["description"] = macro["description"]
-                    row["value"] = macro["value"]
-                    row["type"] = macro["type"]
-                    newHostMacros.append(row)
+        # pick up template bundle
+        templatesToAdd = newHost["templateBundle"].split(';')
+        pprint(templatesToAdd)
 
-            #pprint(newHostMacros)
+        # check if this is ZBX host
+        if newHost["interface_type"]=='1':
+            try:
+                # create a Zabbix agent host
+                print(parse('$.result').find(json.loads(requests.request("POST", url, headers=headers, data=json.dumps({"jsonrpc":"2.0",
+                    "method": "host.create",
+                    "params": {
+                        "host": newHost["hostName"],
+                        "interfaces":[{"type":1,"main":1,"useip":1,"ip":newHost["interface_ip"],"dns":newHost["interface_dns"],"port":newHost["interface_port"]}],
+                        "groups": [{"groupid":"5"}],
+                        "macros": newHostMacros
+                        },
+                "auth": token,"id": 1}), verify=False).text))[0].value)
+                existingHostsList.append(newHost["hostName"])
+            except:
+                print("unable to create ZBX host")
 
-            # check if this is ZBX host
-            if newHost["interface_type"]=='1':
+            
+        elif newHost["interface_type"]=='2':
+            print("new host is SNMP")
+            # this is SNMP host. Need to check version
+
+            if newHost["version"]=='2':
+                # this is SNMPv2 host
                 try:
-                    # create a Zabbix agent host
-                    print(parse('$.result').find(json.loads(requests.request("POST", url, headers=headers, data=json.dumps({
- "jsonrpc": "2.0",
-    "method": "host.create",
-    "params": {
-        "host": newHost["hostName"],
-        "interfaces": [
-            {
-                "type": 1,
-                "main": 1,
-                "useip": 1,
-                "ip": newHost["interface_ip"],
-                "dns": newHost["interface_dns"],
-                "port": newHost["interface_port"]
-            }
-        ],
-        "groups": [
-            {
-                "groupid": "5"
-            }
-        ],
-        "macros": newHostMacros
-    },
-    "auth": token,
-    "id": 1
-                  }), verify=False).text))[0].value)
+                    # create a SNMPv3 host. SNMPv3 host has 9 characteristics
+                    print(parse('$.result').find(json.loads(requests.request("POST", url, headers=headers, data=json.dumps({"jsonrpc": "2.0",
+                        "method": "host.create",
+                        "params": {
+                            "host": newHost["hostName"],
+                            "interfaces":[{"type":2,"main":1,"useip":1,"ip":newHost["interface_ip"],"dns": newHost["interface_dns"],"port": newHost["interface_port"],"details":{"community": newHost["community"],"bulk": newHost["bulk"],"version":newHost["version"]}}],
+                            "groups": [{"groupid": "5"}],
+                            "macros": newHostMacros
+                            },
+                    "auth": token,"id": 1}), verify=False).text))[0].value)
                     existingHostsList.append(newHost["hostName"])
                 except:
-                    print("unable to create ZBX host")
+                    print("unable to create SNMPv2 host")
 
-                
-            elif newHost["interface_type"]=='2':
-                print("new host is SNMP")
-                # this is SNMP host. Need to check version
-
-                if newHost["version"]=='2':
-                    # this is SNMPv2 host
-                    try:
-                        # create a SNMPv3 host. SNMPv3 host has 9 characteristics
-                        print(parse('$.result').find(json.loads(requests.request("POST", url, headers=headers, data=json.dumps({
- "jsonrpc": "2.0",
-    "method": "host.create",
-    "params": {
-        "host": newHost["hostName"],
-        "interfaces": [
-            {
-                "type": 2,
-                "main": 1,
-                "useip": 1,
-                "ip": newHost["interface_ip"],
-                "dns": newHost["interface_dns"],
-                "port": newHost["interface_port"],
-                "details" : {
-                    "community": newHost["community"],
-                    "bulk": newHost["bulk"],
-                    "version": newHost["version"]
-                    }
-            }
-        ],
-        "groups": [
-            {
-                "groupid": "5"
-            }
-        ],
-        "macros": newHostMacros
-    },
-    "auth": token,
-    "id": 1
-                  }), verify=False).text))[0].value)
-                        existingHostsList.append(newHost["hostName"])
-                    except:
-                        print("unable to create SNMPv2 host")
-
-                elif newHost["version"]=='3':
-                    # this is SNMPv3 host
-                    try:
-                        # create a SNMPv3 host. SNMPv3 host has 9 characteristics
-                        print(parse('$.result').find(json.loads(requests.request("POST", url, headers=headers, data=json.dumps({
- "jsonrpc": "2.0",
-    "method": "host.create",
-    "params": {
-        "host": newHost["hostName"],
-        "interfaces": [
-            {
-                "type": 2,
-                "main": 1,
-                "useip": 1,
-                "ip": newHost["interface_ip"],
-                "dns": newHost["interface_dns"],
-                "port": newHost["interface_port"],
-                "details" : {
-                    "version": newHost["version"],
-                    "bulk": newHost["bulk"],
-                    "securityname": newHost["securityname"],
-                    "contextname": newHost["contextname"],
-                    "securitylevel": newHost["securitylevel"],
-                    "authpassphrase": newHost["authpassphrase"],
-                    "authprotocol": newHost["authprotocol"],
-                    "privpassphrase": newHost["privpassphrase"],
-                    "privprotocol": newHost["privprotocol"]
-                    }
-            }
-        ],
-        "groups": [
-            {
-                "groupid": "5"
-            }
-        ],
-        "macros": newHostMacros
-    },
-    "auth": token,
-    "id": 1
-                  }), verify=False).text))[0].value)
-                        existingHostsList.append(newHost["hostName"])
-                    except:
-                        print("unable to create SNMPv3 host")
-                else:
-                    unknownSNMPversion=1
-
+            elif newHost["version"]=='3':
+                # this is SNMPv3 host
+                try:
+                    # create a SNMPv3 host. SNMPv3 host has 9 characteristics
+                    print(parse('$.result').find(json.loads(requests.request("POST", url, headers=headers, data=json.dumps({"jsonrpc": "2.0",
+                        "method":"host.create",
+                        "params":{
+                            "host":newHost["hostName"],"interfaces":[{
+                                "type":2,
+                                "main":1,
+                                "useip":1,
+                                "ip":newHost["interface_ip"],
+                                "dns":newHost["interface_dns"],
+                                "port":newHost["interface_port"],
+                                "details":{
+                                    "version":newHost["version"],
+                                    "bulk":newHost["bulk"],
+                                    "securityname":newHost["securityname"],
+                                    "contextname":newHost["contextname"],
+                                    "securitylevel":newHost["securitylevel"],
+                                    "authpassphrase":newHost["authpassphrase"],
+                                    "authprotocol":newHost["authprotocol"],
+                                    "privpassphrase":newHost["privpassphrase"],
+                                    "privprotocol":newHost["privprotocol"]}}],
+                                "groups":[{"groupid":"5"}],
+                                "macros":newHostMacros
+                                },
+                    "auth":token,"id":1}),verify=False).text))[0].value)
+                    existingHostsList.append(newHost["hostName"])
+                except:
+                    print("unable to create SNMPv3 host")
             else:
-                    notMatchinSNMPorZBX=1
+                unknownSNMPversion=1
+
+        else:
+                notMatchinSNMPorZBX=1
 
 listOfHostsCSV.close()
 listOfHostMacrosCSV.close()
